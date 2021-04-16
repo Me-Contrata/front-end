e6bccc97144da4661fdc208e97562d414291693d
Canturilsnaik <italo.cantur@gmail.com>
2021-04-15 23:14:20 -0300

    Feat: Doing home page

---
+++ src/components/HeaderPublic.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <header class="header">
+        <nav class="header__nav">
+          <p>Projeto</p> <!--Adicionar router-link posteriormente-->
+          <p>Projeto</p>
+          <p>Projeto</p>
+        </nav>
+
+        <div class="header__search">
+          <input type="text">
+          <button>Entrar</button>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+  align-items: center;
+  background: transparent;
+  display: flex;
+  justify-content: space-between;
+}
+
+.header__nav {
+  display: flex;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <HeaderPublic/>
+    <main class="main">
+    <div class="main__text">
+      <h2 class="text__subtitle">Sistema</h2>
+      <h1 class="text__title">Me Contrata</h1>
+      <p class="text__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore cum excepturi mollitia atque consectetur dolorum autem repudiandae tenetur nulla aliquid? Expedita magni iusto maxime quisquam debitis adipisci non consectetur sunt.</p>
+      <button class="text__btn"></button>
+    </div>
+      <figure>
+        <img src="../assets/g10.svg" alt="">
+      </figure>
+    </main>
+  </div>
+</template>
+
+<script>
+import HeaderPublic from '../components/HeaderPublic'
+export default {
+  name: 'Home',
+  components : {
+    HeaderPublic,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
