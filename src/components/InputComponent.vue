af0bd3b8291d1cc34c62c8ba023f7a5816a16144
Canturilsnaik <italo.cantur@gmail.com>
2021-04-21 14:11:24 -0300

    Feat: Add input component

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="genericInput">
+    <input type="text"
+      class="genericInput__basic"
+      :placeholder="inputName"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    inputName: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.genericInput__basic {
+  border: 0;
+  border-radius: 10px;
+  padding: 5px 20px;
+}
+</style>
---
+++ src/views/PageLogin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <HeaderPublic/>
+
+    <h1 class="login__title">Login</h1>
+    <form action="" class="login__form">
+      <input-component
+        :input-name="inputEmail"
+      />
+      <input-component
+        :input-name="inputPassword"
+      />
+      <button class="form__btn"
+      >
+        Entrar
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import HeaderPublic from '@/components/HeaderPublic'
+import InputComponent from '@/components/InputComponent'
+export default {
+  data () {
+    return {
+      inputEmail: 'Email',
+      inputPassword: 'Password'
+    }
+  },
+  components: {
+    HeaderPublic,
+    InputComponent
+  }
+}
+</script>
+
+<style scope>
+
+</style>
