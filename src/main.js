bc0fd49326c40a4349d990219599cc732430249e
ItaloCanturil <italo.cantur@gmail.com>
2021-06-30 23:30:38 -0300

    Feat: add axiosInstance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSearch, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -8,6 +10,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faSearch, faSignInAlt)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false
 
